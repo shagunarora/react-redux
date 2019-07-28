de32ccfd3b860c62242c1eec5e0fae1fc2a6839d
shagunarora <shagunarora2601@gmail.com>
2019-07-28 10:29:20 +0530

    without redux

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    age: 21
+  };
+  onAgeUp = () => {
+    this.setState({
+      ...this.state,
+      age: ++this.state.age
+    });
+  };
+  onAgeDown = () => {
+    this.setState({
+      ...this.state,
+      age: --this.state.age
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div>
+          Age: <span>{this.state.age}</span>
+        </div>
+        <button onClick={this.onAgeUp}>Age Up</button>
+        <button onClick={this.onAgeDown}>Age Down</button>
+      </div>
+    );
+  }
+}
+export default App;
